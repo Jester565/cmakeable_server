5b3bead5a01abaa0bd1af0e104f0b252bfd4a240
Jester565 <ajcraig543@gmail.com>
2017-07-08 16:03:54 -0700

    CMake errors fixed

---
+++ AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/Client.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/Client.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/ClientManager.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/ClientManager.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/HeaderManager.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/HeaderManager.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/IPacket.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/IPacket.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/OPacket.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/OPacket.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/PKey.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/PKey.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/PKeyOwner.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/PKeyOwner.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/PacketManager.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/PacketManager.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/Server.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/Server.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/ServicePool.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/ServicePool.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/TCPAcceptor.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/TCPAcceptor.cpp.o"
+  "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/src/TCPConnection.cpp" "/home/ajcra/northrop/cmakeable_server/AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/src/TCPConnection.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/ajcra/Downloads/install/include"
+  "/usr/local/src/protobuf-3.3.0/install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AsyncServerParent/build/CMakeFiles/AsyncServerParent.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AsyncServerParent.dir/src/Client.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/ClientManager.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/HeaderManager.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/IPacket.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/OPacket.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/PacketManager.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/PKey.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/PKeyOwner.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/Server.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/ServicePool.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/TCPAcceptor.cpp.o"
+  "CMakeFiles/AsyncServerParent.dir/src/TCPConnection.cpp.o"
+  "libAsyncServerParent.pdb"
+  "libAsyncServerParent.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AsyncServerParent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
