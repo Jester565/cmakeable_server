c2f850497f8a83d29f9b47b47c8ea491da817e9b
Cloud User <ec2-user@ip-172-31-7-8.us-west-2.compute.internal>
2017-07-10 03:23:06 -0400

    Can now build example

--- Example/src/source.cpp
+++ Example/src/source.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "WSServer.h"
 
 int main() {
-	std::cout << "Running" << std::endl;
+  WSServer* server = new WSServer(boost::asio::ip::tcp::v6());
+  server->createManagers();
+  server->run(5231);
+  std::cout << "Running" << std::endl;
+  std::string str;
+  std::cin >> str;
 }
